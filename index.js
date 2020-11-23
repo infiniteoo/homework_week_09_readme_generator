b7c103990e716fe7d36e4f51163a09fdae43cf05
Troy Dorman <troydorman@gmail.com>
2020-11-22 19:11:38 -0600

    created javascript file and added inquirer require

---
+++ index.js
@@ -0,0 +1,12 @@
+/* -------------------------------------------
+
+README GENERATOR V.01
+TROY DORMAN
+UNiVERSiTY OF MiNNESOTA CODiNG BOOTCAMP
+FALL 2020
+--------------------------------------------*/
+
+// reference to prompt generating add-on
+const inquirer = require('inquirer');
+
+
