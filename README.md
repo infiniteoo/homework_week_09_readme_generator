72f609a04837943b15d99034c7d38f77fc7f8c8c
Troy Dorman <troydorman@gmail.com>
2020-11-23 19:31:39 -0600

    trying to get the table of contents working in the readme

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
 
-# ggghh
-[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+# dfgd
+[![License: CC0-1.0](https://img.shields.io/badge/License-CC0%201.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)
+#
+## TABLE OF CONTENTS
+* [Description](##DESCRIPTION)
+* [Installation](##INSTALLATION)
+* [Usage Instructions](##USAGE)
+* [Contributing Shoutouts](##CONTRIBUTING)
+* [Test Instructions](##TESTS)
+* [Questions | Contact](##QUESTIONS)
+* [License Information](##LICENSE)
+* [Installation](#installation)
 
-## DESCRIPTION
-dfg
+##DESCRIPTION
+dafgadfg
 
-## INSTALLATION
-sdfg
+##INSTALLATION
+agagsgasg
 
-## USAGE
-ag
+##USAGE
+afgadfg
 
-## CONTRIBUTING
-ag
+##CONTRIBUTING
+dafgadg
 
-## TESTS
-asdg
+##TESTS
+adfgadfg
 
-## QUESTIONS
-### https://github.com/adfhadfh
-#### EMAIL ADDRESS: ahah
-## LICENSE
-The Unilicense
+##QUESTIONS
+###https://github.com/adfhadfh
+####EMAIL ADDRESS: jdfja
+##LICENSE
+Creative Commons Zero v1.0 Universal
--- readme.js
+++ readme.js
@@ -1,32 +1,47 @@
+// grab the answers from the inquirer object in index.js
 const answers = require('./index');
+
+// grab the license map
 const licenseMap = require('./licenses');
 
+// assign the license badge URL to a string to place in the file file
 let licenseBadgeLink = licenseMap.get(answers.licenseEntered);
 
+// template literal string to customize with variable inserts
 const readme = 
 `
 # ${answers.projectTitle}
 ${licenseBadgeLink}
+#
+## TABLE OF CONTENTS
+* [Description](##DESCRIPTION)
+* [Installation](##INSTALLATION)
+* [Usage Instructions](##USAGE)
+* [Contributing Shoutouts](##CONTRIBUTING)
+* [Test Instructions](##TESTS)
+* [Questions | Contact](##QUESTIONS)
+* [License Information](##LICENSE)
+* [Installation](#installation)
 
-## DESCRIPTION
+##DESCRIPTION
 ${answers.descriptionEntered}
 
-## INSTALLATION
+##INSTALLATION
 ${answers.installationEntered}
 
-## USAGE
+##USAGE
 ${answers.usageEntered}
 
-## CONTRIBUTING
+##CONTRIBUTING
 ${answers.contributingEntered}
 
-## TESTS
+##TESTS
 ${answers.testsEntered}
 
-## QUESTIONS
-### https://github.com/${answers.githubUsername}
-#### EMAIL ADDRESS: ${answers.emailEntered}
-## LICENSE
+##QUESTIONS
+###https://github.com/${answers.githubUsername}
+####EMAIL ADDRESS: ${answers.emailEntered}
+##LICENSE
 ${answers.licenseEntered}
 `
 
